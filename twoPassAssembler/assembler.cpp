399a044dedf43be41b45238448cf364ad971ee08
codeIgy <igorblazic6@gmail.com>
2021-08-13 22:14:42 +0200

    Comment removal and label extraction configured.

--- twoPassAssembler/assembler.cpp
+++ twoPassAssembler/assembler.cpp
@@ -2,6 +2,8 @@
 
 #include "assembler.h"
 #include "assemblerException.h"
+#include "lex.h"
+
 Assembler::Assembler()
 {
 }
@@ -23,8 +25,26 @@ void Assembler::assemble(ifstream& inputFile, ofstream& outputFile) {
 
 void Assembler::passFirstTime(ifstream& inputFile) {
 	bool endReached = false;
+	int lineNumber = 0;
 	string line;
 	while (!endReached) {
 		getline(inputFile, line);
+
+		lineNumber++;
+
+		//remove line comment if exists
+		int commentStart = line.find('#');
+		if (commentStart) {
+			line.erase(commentStart, line.size() - commentStart);
+		}
+
+		//check for label
+		smatch labelName;
+		if (regex_search(line, labelName, RegExpr::label)) {
+			string newLabel = labelName[1];
+			line.erase(0, line.find(':') + 1);
+		}
+
+		if (line.empty()) continue; //nothing left for us to analyze
 	}
 }
