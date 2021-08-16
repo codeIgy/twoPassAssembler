8bd3e6c325191c6e52e83670ecbc374437d2e676
codeIgy <igorblazic6@gmail.com>
2021-08-16 22:07:21 +0200

    Added regular expressions for assembly instructions.

--- twoPassAssembler/lex.cpp
+++ twoPassAssembler/lex.cpp
@@ -13,3 +13,31 @@ regex RegExpr::value("0|-?[1-9]\\d*|0[xX][a-fA-F0-9]+");
 regex RegExpr::hex("0[xX][a-fA-F0-9]+");
 
 regex RegExpr::label("^\\s*([A-Za-z_]\\w*)\\s*:.*$");
+
+
+ regex RegExpr::halt("^\\s*halt\\s*$");
+ regex RegExpr::interrupt("^\\s*int\\s+(r[0-7]|sp|pc|psw)\\s*$");
+ regex RegExpr::iret("^\\s*iret\\s*$");
+ regex RegExpr::callFixed("^\\s*call\\s+");
+ regex RegExpr::ret("^\\s*ret\\s*$");
+ regex RegExpr::jmpFixed("^\\s*jump\\s+");
+ regex RegExpr::jeqFixed("^\\s*jeq\\s+");
+ regex RegExpr::jneFixed("^\\s*jne\\s+");
+ regex RegExpr::jgtFixed("^\\s*jgt\\s+");
+ regex RegExpr::push("^\\s*push\\s+(r[0-7]|sp|pc|psw)\\s*$");
+ regex RegExpr::pop("^\\s*pop\\s+(r[0-7]|sp|pc|psw)\\s*$");
+ regex RegExpr::xchg("^\\s*xchg\\s+(r[0-7]|sp|pc|psw)\\s*,\\s*(r[0-7]|sp|pc|psw)\\s*$");
+ regex RegExpr::add("^\\s*add\\s+(r[0-7]|sp|pc|psw)\\s*,\\s*(r[0-7]|sp|pc|psw)\\s*$");
+ regex RegExpr::sub("^\\s*sub\\s+(r[0-7]|sp|pc|psw)\\s*,\\s*(r[0-7]|sp|pc|psw)\\s*$");
+ regex RegExpr::mul("^\\s*mul\\s+(r[0-7]|sp|pc|psw)\\s*,\\s*(r[0-7]|sp|pc|psw)\\s*$");
+ regex RegExpr::div("^\\s*div\\s+(r[0-7]|sp|pc|psw)\\s*,\\s*(r[0-7]|sp|pc|psw)\\s*$");
+ regex RegExpr::cmp("^\\s*cmp\\s+(r[0-7]|sp|pc|psw)\\s*,\\s*(r[0-7]|sp|pc|psw)\\s*$");
+ regex RegExpr::logicNot("^\\s*not\\s+(r[0-7]|sp|pc|psw)\\s*$");
+ regex RegExpr::logicAnd("^\\s*and\\s+(r[0-7]|sp|pc|psw)\\s*,\\s*(r[0-7]|sp|pc|psw)\\s*$");
+ regex RegExpr::logicOr("^\\s*or\\s+(r[0-7]|sp|pc|psw)\\s*,\\s*(r[0-7]|sp|pc|psw)\\s*$");
+ regex RegExpr::logicXor("^\\s*xor\\s+(r[0-7]|sp|pc|psw)\\s*,\\s*(r[0-7]|sp|pc|psw)\\s*$");
+ regex RegExpr::test("^\\s*test\\s+(r[0-7]|sp|pc|psw)\\s*,\\s*(r[0-7]|sp|pc|psw)\\s*$");
+ regex RegExpr::shl("^\\s*shl\\s+(r[0-7]|sp|pc|psw)\\s*,\\s*(r[0-7]|sp|pc|psw)\\s*$");
+ regex RegExpr::shr("^\\s*shr\\s+(r[0-7]|sp|pc|psw)\\s*,\\s*(r[0-7]|sp|pc|psw)\\s*$");
+ regex RegExpr::ldrFixed("^\\s*ldr\\s+(r[0-7]|sp|pc|psw)\\s*,\\s*");
+ regex RegExpr::strFixed("^\\s*str\\s+(r[0-7]|sp|pc|psw)\\s*,\\s*");
--- twoPassAssembler/lex.h
+++ twoPassAssembler/lex.h
@@ -24,6 +24,33 @@ public:
 	//label regex
 	static regex label;
 
+	//assembly instructions
+	static regex halt;
+	static regex interrupt;
+	static regex iret;
+	static regex callFixed;
+	static regex ret;
+	static regex jmpFixed;
+	static regex jeqFixed;
+	static regex jneFixed;
+	static regex jgtFixed;
+	static regex push;
+	static regex pop;
+	static regex xchg;
+	static regex add;
+	static regex sub;
+	static regex mul;
+	static regex div;
+	static regex cmp;
+	static regex logicNot;
+	static regex logicAnd;
+	static regex logicOr;
+	static regex logicXor;
+	static regex test;
+	static regex shl;
+	static regex shr;
+	static regex ldrFixed;
+	static regex strFixed;
 };
 
 #endif
